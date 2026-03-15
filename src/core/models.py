0f35c071d0cfe1b1d01985d6164f5825f07a3b0b
Sivashankar <myowndevtest@gmail.com>
2026-03-16 00:54:29 +0530

    feat: Add auto-updating 'updated' timestamp field to Book model.

--- src/core/models.py
+++ src/core/models.py
@@ -5,6 +5,7 @@ class Actor(models.Model):
     name = models.CharField(max_length=128)
     nationality = models.CharField(max_length=128)
     created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
     released = models.DateField(null=True, blank=True)
     def __str__(self):
         return self.name
